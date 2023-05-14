cdef8a3b7c74d8669e9d047af5fc7d91fc8032d7
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-05-13 22:23:23 -0300

    :sparkles: add info visualization into dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AirportComponent } from './airport/airport.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, AirportComponent],
   imports: [CommonModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { FlightService } from 'src/app/services/flights.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  airports: any[] = [];
+  date = new Date(this.flightService.beginTime);
+  constructor(private flightService: FlightService) {}
 
   ngOnInit(): void {
+    this.flightService
+      .getFlightsByTimeInterval()
+      .pipe(
+        map((flight: any) => {
+          return flight.map((flight: any) => ({
+            estDepartureAirport: flight.estDepartureAirport,
+            estArrivalAirport: flight.estArrivalAirport,
+          }));
+        })
+      )
+      .subscribe((flights: any) => {
+        const airportsSet = new Set();
+        flights.forEach((flight: any) => {
+          airportsSet.add(flight.estDepartureAirport);
+          airportsSet.add(flight.estArrivalAirport);
+        });
+        airportsSet.delete(null);
+        this.airports = Array.from(airportsSet);
+      });
   }
-
 }
